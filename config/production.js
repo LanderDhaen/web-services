2e67c05070bdc706bad8f9a79759b32402fe2dca
Lander <lander.dhaen@student.hogent.be>
2023-12-22 16:41:57 +0100

    - Bugfix deployment

--- config/production.js
+++ config/production.js
@@ -1,8 +1,13 @@
 module.exports = {
-  port: 9000,
   log: {
     level: "info",
     disabled: false,
+  },
+  database: {
+    client: "mysql2",
+    host: "localhost",
+    port: 3306,
+    name: "Webservices",
   },
   cors: {
     origins: ["http://localhost:5173"],
